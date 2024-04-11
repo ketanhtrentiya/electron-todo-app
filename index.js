401faaa8ae8dfa29d70b097d8d28857c35206731
Ketan Trentiya <ketanhtrentiya@gmail.com>
2024-04-11 16:20:45 +0530

    Add sub window feature

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const electron = require("electron");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
+
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
@@ -13,7 +15,19 @@ app.on("ready", () => {
   mainWindow.loadURL(`file://${__dirname}/main.html`);
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
+
+  // Handle Application Close Event
+  mainWindow.on('closed', () => app.quit());
 });
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Todo"
+  });
+  addWindow.loadURL(`file://${__dirname}/views/add.html`);
+}
 
 const menuTemplate = [
   {
@@ -21,6 +35,9 @@ const menuTemplate = [
     submenu: [
       {
         label: "New Todo",
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: "Quit",
